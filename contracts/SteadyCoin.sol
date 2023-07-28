704134089b2c18f35074433211282ff0d04d57db
ricobl4ck <adrishahri@gmail.com>
2023-07-28 22:11:40 +0800

    add: ERC20 contract

---
+++ contracts/SteadyCoin.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/*
+ * @title SteadyCoin
+ * @author ricogustavo
+ * @team Futurify x EpicStartups
+ *
+ */
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SteadyCoin is ERC20Burnable, Ownable {
+    error SteadyCoin__AmountMustBeMoreThanZero();
+    error SteadyCoin__BurnAmountExceedsBalance();
+    error SteadyCoin__NotZeroAddress();
+    error SteadyCoin__AddressesAndAmountsMustBeEqualLength();
+
+    event SteadyCoin__Burn(address indexed burner, uint256 amount);
+    event SteadyCoin__Mint(address indexed to, uint256 amount);
+
+    constructor() ERC20("SteadyCoin", "STC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert SteadyCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert SteadyCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+        emit SteadyCoin__Burn(msg.sender, _amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert SteadyCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert SteadyCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        emit SteadyCoin__Mint(_to, _amount);
+        return true;
+    }
+}
