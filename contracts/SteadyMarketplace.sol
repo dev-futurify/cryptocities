14c4ff1fb13cab085da99c2ab653e68860081c62
ricobl4ck <adrishahri@gmail.com>
2023-08-08 21:50:59 +0800

    chores: prettier

--- contracts/SteadyMarketplace.sol
+++ contracts/SteadyMarketplace.sol
@@ -12,9 +12,7 @@ pragma solidity 0.8.19;
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {SellOrderSetLib} from "./libraries/SellOrderSetLib.sol";
 
