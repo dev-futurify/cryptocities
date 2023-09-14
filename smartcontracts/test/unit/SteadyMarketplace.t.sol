b30df216d2200d75f4e29be8eb05e9b63759fd4e
ass77 <adrishahri@gmail.com>
2023-09-14 23:26:44 +0800

    init: test marketplace script

--- smartcontracts/test/unit/SteadyMarketplace.t.sol
+++ smartcontracts/test/unit/SteadyMarketplace.t.sol
@@ -13,10 +13,15 @@ import {OrderSet} from "../../contracts/libraries/OrderSet.sol";
 contract SteadyMarketplaceTest is Test {
     using OrderSet for OrderSet.Set;
 
-    SteadyEngine private steadyEngine;
-    SteadyFormula private steadyFormula;
-    SteadyCoin private steadyCoin;
-    SteadyMarketplace private steadyMarketplace;
+    SteadyEngine steadyEngine;
+    SteadyFormula steadyFormula;
+    SteadyCoin steadyCoin;
+    SteadyMarketplace steadyMarketplace;
+
+    string public vendorName;
+    string public vendorDescription;
+    string public vendorBusinessRegistrationNumber;
+    address public vendorAddress = msg.sender;
 
     function setUp() public {
         steadyCoin = new SteadyCoin();
@@ -25,6 +30,56 @@ contract SteadyMarketplaceTest is Test {
             address(steadyMarketplace),
             address(steadyFormula)
         );
-        steadyMarketplace = new SteadyMarketplace(address(steadyFormula));
+    }
+
+    function testRegisterVendor() public {
+        vm.prank(steadyMarketplace.owner());
+
+        vendorName = "Vendor 1";
+        vendorDescription = "Vendor 1 description";
+        vendorBusinessRegistrationNumber = "1234567890";
+
+        // Register the vendor
+        steadyMarketplace.registerVendor{value: 100 ether}(
+            vendorName,
+            vendorDescription,
+            vendorBusinessRegistrationNumber
+        );
+
+        vm.expectRevert();
+
+        // Fetch the vendor's name using the getVendorByAddress function
+        address addr = steadyMarketplace
+            .getVendorByAddress(vendorAddress)
+            .vendorAddress;
+
+        string memory name = steadyMarketplace
+            .getVendorByAddress(vendorAddress)
+            .vendorName;
+
+        string memory description = steadyMarketplace
+            .getVendorByAddress(vendorAddress)
+            .vendorDescription;
+
+        string memory businessRegistrationNumber = steadyMarketplace
+            .getVendorByAddress(vendorAddress)
+            .vendorBusinessRegistrationNumber;
+
+        uint256 dateCreated = steadyMarketplace
+            .getVendorByAddress(vendorAddress)
+            .dateCreated;
+
+        uint256 dateUpdated = steadyMarketplace
+            .getVendorByAddress(vendorAddress)
+            .dateUpdated;
+
+        assertEq(addr, vendorAddress);
+        assertEq(name, vendorName);
+        assertEq(description, vendorDescription);
+        assertEq(businessRegistrationNumber, vendorBusinessRegistrationNumber);
+        assertEq(dateCreated, dateCreated);
+        assertEq(dateUpdated, dateUpdated);
+
+        vm.stopPrank();
     }
 }
