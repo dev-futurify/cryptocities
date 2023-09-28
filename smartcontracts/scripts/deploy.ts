27d8a00ba82b9aab73690a3b5992cd4040f3fbe5
ass77 <adrishahri@gmail.com>
2023-09-28 21:51:36 +0800

    update: deployment script - hardhat

--- smartcontracts/scripts/deploy.ts
+++ smartcontracts/scripts/deploy.ts
@@ -1,21 +1,27 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const steadyCoin = await ethers.deployContract("SteadyCoin");
-  await steadyCoin.waitForDeployment();
+  console.log("Initializing deployment...");
 
+  const steadyCoin = await ethers.deployContract("SteadyCoin");
   console.log(`SteadyCoin deployed to ${steadyCoin.address}`);
 
-  const steadyMarketplace = await ethers.deployContract("SteadyMarketplace");
+  const steadyFormula = await ethers.deployContract("SteadyFormula");
+  console.log(`SteadyFormula deployed to ${steadyFormula.address}`);
 
+  const steadyMarketplace = await ethers.deployContract("SteadyMarketplace", [
+    steadyFormula.address,
+  ]);
   console.log(`SteadyMarketplace deployed to ${steadyMarketplace.address}`);
 
   const steadyEngine = await ethers.deployContract("SteadyEngine", [
     steadyCoin.address,
     steadyMarketplace.address,
+    steadyFormula.address,
   ]);
-
   console.log(`SteadyEngine deployed to ${steadyEngine.address}`);
+
+  console.log("Deployment completed!");
 }
 
 main().catch((error) => {
